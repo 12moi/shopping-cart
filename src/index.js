eba657547a628dd16205f086bd3ad4d6c871e654
devmoi <moishadrack.com>
2022-11-30 21:35:11 +0300

    add project initial files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import App from './App'
+import GlobalStyle from './theme/GlobalStyle'
+import Theme from './theme/Theme'
+import store from './state/store/store'
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const root = ReactDOM.createRoot( document.getElementById("root") );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>,
+ 
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
