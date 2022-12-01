cada663a63b92be913d77c2ef5abc7e9b2045d9b
devmoi <moishadrack.com>
2022-12-01 09:42:11 +0300

    add product section

---
+++ src/pages/Contact.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import styled from 'styled-components'
+import {
+  FaGithub,
+  FaFacebookMessenger,
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+} from 'react-icons/fa'
+
+const Contact = () => {
+  return (
+    <ContactWrapper>
+      <SocialMedia>
+        <SocialLink href="https://github.com/12moi" target="_blank">
+          <FaGithub />
+        </SocialLink>
+        <SocialLink
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+        >
+          <FaFacebookF />
+        </SocialLink>
+        <SocialLink
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+        >
+          <FaFacebookMessenger />
+        </SocialLink>
+        <SocialLink
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+        >
+          <FaTwitter />
+        </SocialLink>
+        <SocialLink
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+        >
+          <FaInstagram />
+        </SocialLink>
+      </SocialMedia>
+      <ContactInfo>
+        <p>FakeStore</p>
+        <p>Nairobi, Kenya</p>
+      </ContactInfo>
+    </ContactWrapper>
+  )
+}
+
+const ContactWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10rem;
+  margin-top: 14rem;
+  margin-bottom: 26.5rem;
+
+  animation: fadeIn ease 2s;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  @media (max-width: 480px) {
+    margin-top: 5rem;
+  }
+`
+
+const SocialMedia = styled.div`
+  display: flex;
+  gap: 10rem;
+  font-size: 10rem;
+
+  @media (max-width: 768px) {
+    gap: 5rem;
+    font-size: 8rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 5rem;
+  }
+`
+
+const ContactInfo = styled.div`
+  font-size: 3rem;
+  font-weight: bold;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 2.4rem;
+  }
+`
+
+const SocialLink = styled.a`
+  cursor: pointer;
+  transition: transform 0.15s ease-in-out;
+  color: ${({ theme }) => theme.colors.dark};
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+export default Contact
---
+++ src/pages/Products.js
@@ -0,0 +1,77 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { v4 as uuidv4 } from 'uuid'
+import { useDispatch, useSelector } from 'react-redux'
+import ProductCard from './Products/ProductCard'
+import { setProducts } from '../state/actions/products'
+
+const Products = () => {
+  const products = useSelector((state) => state.products)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    loadProducts()
+   
+  // eslint-disable-next-line no-use-before-define
+  }, [loadProducts])
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const loadProducts = async () => {
+    dispatch(setProducts(filterProducts(await fetchProducts())))
+  }
+
+  const fetchProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products')
+    let data = await response.json()
+    return data
+  }
+
+  const filterProducts = (products) => {
+    return products.filter(
+      (product) =>
+        product.category === `men's clothing` ||
+        product.category === `women's clothing`
+    )
+  }
+
+  const productCards = products.map((product) => (
+    <ProductCard
+      key={uuidv4()}
+      id={product.id}
+      title={product.title}
+      price={product.price}
+      image={product.image}
+    />
+  ))
+
+  return <ProductsWrapper>{productCards}</ProductsWrapper>
+}
+
+const ProductsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 4rem;
+  margin-top: 4rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, minmax(28rem, 36rem));
+    justify-content: center;
+  }
+
+  @media (max-width: 480px) {
+    grid-template-columns: repeat(1, 36rem);
+  }
+
+  animation: fadeIn ease 2s;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`
+
+export default Products
