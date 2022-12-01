60d063febb3a591bf6e5ca696a1dd9ec321c85dd
devmoi <moishadrack.com>
2022-12-01 13:08:48 +0300

    add themes

---
+++ src/theme/theme.js
@@ -0,0 +1,31 @@
+const Theme = {
+    colors: {
+      primary: '#46FFD3',
+      red: '#ff9999',
+      dark: '#000',
+      light: '#FFF',
+      grey: {
+        light: '#F9F9F9',
+        main: '#E5E5E5',
+        dark: '#7B7B7B',
+      },
+      hover: {
+        primary: '#35eec2',
+        red: '#ee8888',
+        grey: '#d4d4d4',
+        dark: '#333',
+      },
+      active: {
+        primary: '#24ddb1',
+        red: '#dd7777',
+        grey: '#c3c3c3',
+        dark: '#444',
+      },
+    },
+  
+    widths: {
+      content: '140rem',
+    },
+  }
+  
+  export default Theme
